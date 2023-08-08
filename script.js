624cca18f85a38364a5a252833588830a4844c13
GiroSan <girosan86@gmail.com>
2023-08-08 14:20:36 -0300

    Aula sete completa

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const outroY = 5
 
 const final = x + outroY + 7 + outroW
 console.log(final)
+
+const serie = {
+    nome:"Simpsons",
+    temporadas: 12
+}
+
+serie.temporadas = 9
+serie.episodios = 50 
+const frase = `A série ${serie.nome} tem ${serie.temporadas} temporadas.`
+const totalEpisodios = serie.temporadas *  serie.episodios
+
+console.log(serie)
+console.log(totalEpisodios)
+
+//Objetos em Javascript são mutaveis por padrão.
+
+const pessoa = {
+    nome: 'Giro',
+    idade: 37,
+    peso:110,
+    altura:1.83,
+}
+console.log(pessoa)
